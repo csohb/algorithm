4792ec2d1b0760354b7adb8f0aeffae21598b0c1
winter.kwon <winter.kwon@gbike.io>
2024-01-13 14:58:55 +0900

    mergeIntervals 추가

--- coderpad/77/main.go
+++ coderpad/77/main.go
@@ -9,8 +9,8 @@ func mergeIntervals(givenArray [][]int) (sortedArray [][]int) {
 	sortedArray = append(sortedArray, givenArray[0])
 
 	for i := 1; i < len(givenArray); i++ {
-		lastIntervalIndexOfSortedArray := len(sortedArray) - 1
-		if sortedArray[lastIntervalIndexOfSortedArray][1] < givenArray[i][0] {
+		lastIndex := len(sortedArray) - 1
+		if sortedArray[lastIndex][1] < givenArray[i][0] {
 			sortedArray = append(sortedArray, givenArray[i])
 		}
 	}
@@ -20,18 +20,25 @@ func mergeIntervals(givenArray [][]int) (sortedArray [][]int) {
 func main() {
 	data := [][]int{
 		{1, 3},
-		{5, 8},
+		{30, 8},
 		{4, 10},
 		{20, 25},
 	}
 
 	fmt.Printf("input: %+v\n", data)
 
+	for _, v := range data {
+		sort.Slice(v, func(i, j int) bool {
+			return v[i] < v[j]
+		})
+	}
+
+	fmt.Println("sorted1:", data)
 	sort.Slice(data, func(i, j int) bool {
 		return data[i][0] < data[j][0]
 	})
 
-	fmt.Println("sorted:", data)
+	fmt.Println("sorted2:", data)
 
 	output := mergeIntervals(data)
 	fmt.Println("output:", output)
